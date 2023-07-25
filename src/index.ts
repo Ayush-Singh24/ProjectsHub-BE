d2af2ecec5c793868cc0ef8d564434e6df6028b0
Ayush Singh <asayushsingh80@gmail.com>
2023-07-25 23:11:11 +0530

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+import express from "express";
+
+import cors from "cors";
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}⚡`);
+});
