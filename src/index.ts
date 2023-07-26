a7fe76d75173f45c44bb1de1131e6dbb8c35fbf8
Ayush Singh <asayushsingh80@gmail.com>
2023-07-26 23:20:25 +0530

    setup express-session with prisma and postgres

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,48 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
+
+import { config } from "dotenv";
+config();
 
 import cors from "cors";
 
+export const prisma = new PrismaClient();
+
 const PORT = process.env.PORT || 8000;
-const app = express();
+const app: Express = express();
+
+declare module "express-session" {
+  interface SessionData {
+    user: string;
+  }
+}
 
 app.use(
   cors({
     credentials: true,
   })
 );
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdFunction: undefined,
+      dbRecordIdIsSessionId: true,
+    }),
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  req.session.user = "hello";
+  console.log(req.session);
+  res.status(200).send("hello world");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}⚡`);
